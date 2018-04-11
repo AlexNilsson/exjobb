b700004770d0e8e112cdd1bc3535e34ecccd7f48
Martin <martin.thonners@gmail.com>
2018-04-11 10:17:32 +0200

    added conv. layers
    working on custom training images, place the images to train on in shape folder

--- VariationalAE/VAE.py
+++ VariationalAE/VAE.py
@@ -1,6 +1,6 @@
 
 
-from keras.layers import Input, Dense, Lambda, Dropout
+from keras.layers import Input, Dense, Lambda, Dropout, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape
 from keras.models import Model
 from keras.objectives import binary_crossentropy
 from keras.callbacks import LearningRateScheduler
@@ -15,21 +15,29 @@ import keras.backend as K
 import tensorflow as tf
 import os
 
-pixels_amount = 128
-batches_size= 8
-input_layer_size = pixels_amount*pixels_amount
-hidden_1_size =512
-n_z = 2
-n_epoch = 10
-img_rows, img_cols = pixels_amount, pixels_amount
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
+""" user preferences """
+pixels_amount = 200 #must be dividable by 8
+batches_size= 25 #the trainingset must be dividable with batches_size
+n_epoch = 1
+hidden_1_size =4 #the flat dense layers before and after z
+z_layer_size = 2
 
+input_layer_size = pixels_amount*pixels_amount
+img_rows, img_cols = pixels_amount, pixels_amount #what size images are reshaped to
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
+
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
 #defining and reshaping data
 train_images_path = os.path.join(os.path.dirname(__file__), '../data/shapes/shape')
 #print(train_images_path)
 train_images_resized_path = os.path.join(os.path.dirname(__file__), '../data/shapes/shape_resized')
 
-
-#resizing and grayscale
+""" resizing and grayscale of training images """ 
 listing = os.listdir(train_images_path)
 num_samples = np.size(listing)
 
@@ -48,81 +56,151 @@ immatrix = np.array([np.array(Image.open(train_images_resized_path + '\\' + im2)
     for im2 in imlist], 'f')
 
 print(immatrix.shape)
-#--------------------------------------------------------------------
-#----------------------------------------------
-""" (x_train, y_train), (x_test, y_test) = mnist.load_data() """
+
 x_train = immatrix
 x_test = immatrix
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+x_train = np.reshape(x_train, (len(x_train), pixels_amount, pixels_amount, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), pixels_amount, pixels_amount, 1))  # adapt this if using `channels_first` image data format
 print(x_train.shape)
 print(x_test.shape)
 
-
-""" y_train = y_train.astype('float32') / 255.
-y_test = y_test.astype('float32') / 255. """
-
-#if noisy images is wanted
+""" #if noisy images is wanted
 noise_factor = 0.4
 x_train_noisy = x_train + noise_factor * np.random.normal(size=x_train.shape) 
 x_test_noisy = x_test + noise_factor * np.random.normal(size=x_test.shape)
 
 x_train_noisy = np.clip(x_train_noisy, 0.0, 1.0)
-x_test_noisy = np.clip(x_test_noisy, 0.0, 1.0)
+x_test_noisy = np.clip(x_test_noisy, 0.0, 1.0) """
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
 
-#encoder
-inputs = Input(shape=(input_layer_size,))
-h_q =Dense(hidden_1_size, activation='relu')(inputs)
-mu = Dense(n_z, activation='linear')(h_q)
-log_sigma = Dense(n_z, activation='linear')(h_q)
-
-#reparametrization trick 
-def sample_z(args):
-    mu, log_sigma = args
-    eps = K.random_normal(shape=(batches_size, n_z), mean=0., stddev=1.)
-    return mu + K.exp(log_sigma / 2) * eps
-#sample z Q(z|X)
-z = Lambda(sample_z)([mu, log_sigma])
-
-#decoder -- P(X|z)
-decoder_hidden = Dense(hidden_1_size, activation='relu')
-dropout_layer = Dropout(0.2, input_shape=(hidden_1_size,))
-decoder_out = Dense(input_layer_size, activation='sigmoid')
-
-h_p = decoder_hidden(z)
-outputs = decoder_out(h_p)
-
-#overall VAE model, for reconstruction and training
-vae = Model(inputs, outputs)
 
 #encoder model, to encode input into latent variable
+""" Convolution layers """
+conv_1 = Conv2D(16, (3, 3), activation='relu', padding='same')
+pooling_1 = MaxPooling2D((2, 2), padding='same')
+conv_2 = Conv2D(8, (3, 3), activation='relu', padding='same')
+pooling_2 = MaxPooling2D((2, 2), padding='same')
+conv_3 = Conv2D(8, (3, 3), activation='relu', padding='same')
+pooling_3 = MaxPooling2D((2, 2), padding='same')
+
+""" Flat layers """
+flat_1 =Dense(hidden_1_size, activation='relu')
+flat_2 = Dense(z_layer_size, activation='linear')
+flat_3 = Dense(z_layer_size, activation='linear')
+
+""" Structure """
+inputs = Input(shape=(pixels_amount, pixels_amount, 1))
+x = conv_1(inputs)
+x = pooling_1(x)
+x = conv_2(x)
+x = pooling_2(x)
+x = conv_3(x)
+convoluted = pooling_3(x)
+
+convoluted_flat = Flatten()(convoluted)
+h_q =flat_1(convoluted_flat)
+mu = flat_2(h_q)
+log_sigma = flat_3(h_q)
 encoder = Model(inputs, mu)
 
-#decoder (generator) model, to generate new data given variable z
-d_in = Input(shape=(n_z,))
-d_h = decoder_hidden(d_in)
-drop = dropout_layer(d_h )
-d_out = decoder_out(drop)
-decoder = Model(d_in, d_out)
+print('encoder summary')
+encoder.summary()
+
+""" Variables used for decoders layers sizes """
+convShape = convoluted.shape
+_x = int(convShape[1])
+_z = int(convShape[3])
+
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
+#reparametrization trick to sample z 
+def sample_z(args):
+    mu, log_sigma = args
+    eps = K.random_normal(shape=(K.shape(mu)[0], z_layer_size), mean=0., stddev=1.)
+    return mu + K.exp(log_sigma / 2) * eps
+
+""" def sample_z(mu, log_sigma):
+    # we sample from the standard normal a matrix of batch_size * latent_size (taking into account minibatches)
+    eps = K.random_normal(shape=(K.shape(mu)[0], z_layer_size), mean=0, stddev=1)
+    # sampling from Z~N(μ, σ^2) is the same as sampling from μ + σX, X~N(0,1)
+    return mu + K.exp(log_sigma) * eps """
+
+#sample z Q(z|X)
+z = Lambda(sample_z)([mu, log_sigma])
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
+
+
+#decoder -- P(X|z) (generator) model, to generate new data given variable z
+""" Layers """
+dropout_layer = Dropout(0.2, input_shape=(hidden_1_size,)) #can be placed between the flat dense layers if needed
+decoder_hidden = Dense(hidden_1_size, activation='relu')
+d_h = Dense(_x*_x*_z, activation='relu')
+d_h_reshaped = Reshape((_x,_x,_z))
+decon_1 = Conv2D(8, (3, 3), activation='relu', padding='same')
+upsamp_1 = UpSampling2D((2, 2))
+decon_2 = Conv2D(8, (3, 3), activation='relu', padding='same')
+upsamp_2 = UpSampling2D((2, 2))
+decon_3 = Conv2D(16, (3, 3), activation='relu', padding='same')
+upsamp_3 = UpSampling2D((2, 2))
+decon_4 = Conv2D(1, (3, 3), activation='sigmoid', padding='same')
+
+""" Structure """
+d_in = Input(shape=(z_layer_size,))
+x = decoder_hidden(d_in)
+x = d_h(x)
+x = d_h_reshaped(x)
+x = decon_1(x)
+x = upsamp_1(x)
+x = decon_2(x)
+x = upsamp_2(x)
+x = decon_3(x)
+x = upsamp_3(x)
+decode_output = decon_4(x)
+decoder = Model(d_in, decode_output)
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
+
+#VAE model, for reconstruction and training
+""" Structure """
+x = decoder_hidden(z)
+x = d_h(x)
+x = d_h_reshaped(x)
+x = decon_1(x)
+x = upsamp_1(x)
+x = decon_2(x)
+x = upsamp_2(x)
+x = decon_3(x)
+x = upsamp_3(x)
+outputs = decon_4(x)
+vae = Model(inputs, outputs)
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
 
 #loss definition
-""" def vae_loss(y_true, y_pred):
-    calculate loss = reconstruction loss + KL loss for each data in minibatch
-    #E[log P(X|z)]
-    recon = K.sum(K.binary_crossentropy(y_pred, y_true), axis=1)
-    #D_KL(Q(z|X)) || P(z|X)
-    kl = 0.5*K.sum(K.exp(log_sigma) + K.square(mu) -1. - log_sigma, axis=1)
+""" Flatten input and output layer to use in loss function """
+inputs_flat = Flatten()(inputs)
+outputs_flat = Flatten()(outputs)
 
-    return recon + kl """
+""" loss function """
+def vae_loss(inputs_flat, outputs_flat):
+	# compute the average MSE error, then scale it up, ie. simply sum on all axes
+	reconstruction_loss = K.sum(K.square(outputs_flat-inputs_flat))
+	# compute the KL loss
+	kl_loss = - 0.5 * K.sum(1 + log_sigma - K.square(mu) - K.square(K.exp(log_sigma)), axis=-1)
+	# return the average loss over all images in batch
+	total_loss = K.mean(reconstruction_loss + kl_loss)    
+	return total_loss
 
-def vae_loss(inputs, outputs):
-    xent_loss = input_layer_size*binary_crossentropy(inputs, outputs)
-    kl_loss = - 0.5 * K.mean(1 + log_sigma - K.square(mu) - K.exp(log_sigma), axis=-1)
-    return xent_loss + kl_loss
+print('vae loss')
+print(vae_loss)
 
-#vae.compile(optimizer='rmsprop', loss=vae_loss)
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
 
 #models summary
 print('vae')
@@ -132,34 +210,31 @@ encoder.summary()
 print('decoder')
 decoder.summary()
 
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
 #training
 vae.compile(optimizer='adam', loss=vae_loss)
-#vae.fit(x_train, x_train, batch_size=batches_size, nb_epoch=n_epoch)
-
-vae.fit(x_train_noisy, x_train,
+vae.fit(x_train, x_train,
         shuffle=True,
         epochs=n_epoch,
         batch_size=batches_size,
         validation_data=(x_test, x_test))
 
-""" vae.fit_generator(train_batches, epochs=n_epoch)
- """
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
 """ #saving model and weights
 vae.save('vae_model.h5')
 vae.save_weights('vae_weights.h5')
 decoder.save_weights('decoder_weights.h5')
 encoder.save_weights('encoder_weights.h5') """
 
-""" #plot of class clustering
-x_test_encoded = encoder.predict(x_test, batch_size=m)
-plt.figure(figsize=(6, 6))
-plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1]) #, c=y_test
-plt.colorbar()
-x_test_encoded = encoder.predict_generator(train_batches) """
-# display a 2D manifold of the digits
-n = 15  # figure with 15x15 digits
-digit_size = pixels_amount
-figure = np.zeros((digit_size * n, digit_size * n))
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
+# visualisation
+n = 15  # figure with 15x15 images
+visualisation_size = pixels_amount
+figure = np.zeros((visualisation_size * n, visualisation_size * n))
 # we will sample n points within [-15, 15] standard deviations
 grid_x = np.linspace(-15, 15, n)
 grid_y = np.linspace(-15, 15, n)
@@ -168,11 +243,13 @@ for i, yi in enumerate(grid_x):
     for j, xi in enumerate(grid_y):
         z_sample = np.array([[xi, yi]])
         x_decoded = decoder.predict(z_sample)
-        digit = x_decoded[0].reshape(digit_size, digit_size)
-        figure[i * digit_size: (i + 1) * digit_size,
-               j * digit_size: (j + 1) * digit_size] = digit
+        visualisation_image = x_decoded[0].reshape(visualisation_size, visualisation_size)
+        figure[i * visualisation_size: (i + 1) * visualisation_size,
+               j * visualisation_size: (j + 1) * visualisation_size] = visualisation_image
 
 plt.figure(figsize=(10, 10))
 plt.imshow(figure)
 
 plt.show()
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
