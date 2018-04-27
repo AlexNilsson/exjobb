d1894584443587a75e05a173a6535ee97d2aa0a4
Alexander Nilsson <alexandernilsson12@gmail.com>
2018-04-27 08:25:08 +0200

    added larger data

--- VariationalAE/test_adv_ae.py
+++ VariationalAE/test_adv_ae.py
@@ -2,10 +2,11 @@
 
 from keras.layers import Input, Dense, Lambda, Dropout, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape
 from keras.models import Model
-from keras.objectives import binary_crossentropy
+from keras.objectives import binary_crossentropy, mean_squared_error
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint, Callback
 from keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
+from keras.layers.advanced_activations import LeakyReLU
 
 import pandas as pd
 import cv2 as c
@@ -18,20 +19,28 @@ import os
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 """ user preferences """
-pixels_amount = 28 #must be dividable by 8
-batchsize = 200 #the trainingset must be dividable with batches_size
+pixels_amount = 80 #must be dividable by 8
+batchsize = 500 #the trainingset must be dividable with batches_size
 n_epoch = 10000
-hidden_1_size =100 
-hidden_2_size = 50 #the flat dense layers before and after z
+hidden_1_size =500 
+hidden_2_size = 100 #the flat dense layers before and after z
 dropout_amount = 0.3
 z_layer_size = 2
 
+SAVE_IMAGE_ON_EPOCH = 50
+SAVE_WEIGHTS_ON_EPOCH = 2000
+
+LOAD_WEIGHTS_TO_MODEL=False
+PREVIOUSLY_RUN_EPOCH = 0
+
 """ adjust discriminator layersize to match complexity of the decoder """
-disc_1_size = 50
-disc_2_size = 100
-disc_3_size = 100
-disc_4_size = 50
-disc_5_size = 50
+disc_1_size = 250
+disc_2_size = 500
+disc_3_size = 500
+disc_4_size = 80
+disc_5_size = 80
+
+
 
 input_layer_size = pixels_amount*pixels_amount
 img_rows, img_cols = pixels_amount, pixels_amount #what size images are reshaped to
@@ -39,20 +48,23 @@ img_rows, img_cols = pixels_amount, pixels_amount #what size images are reshaped
 #----------------------------------------------
 
 
-train = pd.read_csv("./data/csv_mnist_gan/train.csv").values
+''' train = pd.read_csv("./data/csv_mnist_gan/train.csv").values
 
 x_train = train[:, 1:].reshape(train.shape[0], 28, 28, 1)
 x_train = x_train.astype(float)
 x_train /= 255.0
 
-x_train = x_train[:2000]
+x_train = x_train[:2000] '''
 
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 #defining and reshaping data
-""" train_images_path = os.path.join(os.path.dirname(__file__), '../data/shapes/shape')
-train_images_resized_path = os.path.join(os.path.dirname(__file__), '../data/shapes/shape_resized')
+''' train_images_path = os.path.join(os.path.dirname(__file__), '../data/shapes/shape')
+train_images_resized_path = os.path.join(os.path.dirname(__file__), '../data/shapes/shape_resized') '''
 train_images_resized_bigger_path = os.path.join(os.path.dirname(__file__), '../data/shapes/shape_resized_bigger')
+
+train_images_path = os.path.join(os.path.dirname(__file__), '../data/windows/original_images')
+train_images_resized_path = os.path.join(os.path.dirname(__file__), '../data/windows/resized_images')
 #resizing and grayscale of training images 
 listing = os.listdir(train_images_path)
 num_samples = np.size(listing)
@@ -62,9 +74,9 @@ for file in listing:
     img = im.resize((img_rows,img_cols))
     gray = img.convert('L')
     gray.save(train_images_resized_path + '\\' + file ) #+ ".jpg"
-    img_re = im.resize((img_rows*2,img_cols*2))
+'''     img_re = im.resize((img_rows*2,img_cols*2))
     gray_re = img_re.convert('L')
-    gray_re.save(train_images_resized_bigger_path + '\\' + file )
+    gray_re.save(train_images_resized_bigger_path + '\\' + file ) '''
 
 imlist = os.listdir(train_images_resized_path)
 im1= np.array(Image.open(train_images_resized_path + '\\' + imlist[0])) #open one image to get size
@@ -72,7 +84,7 @@ m,n = im1.shape[0:2] #size of image
 imnbr = len(imlist) #number of images
 #matrix to store flattened images
 immatrix = np.array([np.array(Image.open(train_images_resized_path + '\\' + im2)).flatten()
-    for im2 in imlist], 'f') """
+    for im2 in imlist], 'f') 
 
 """ imlist = os.listdir(train_images_resized_bigger_path)
 im1= np.array(Image.open(train_images_resized_bigger_path + '\\' + imlist[0])) #open one image to get size
@@ -84,14 +96,14 @@ print(immatrix.shape)
 print(immatrix_bigger.shape) """
 
 """ x_test = immatrix_bigger """
-""" x_train = immatrix
+x_train = immatrix
 x_test = immatrix
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = np.reshape(x_train, (len(x_train), pixels_amount, pixels_amount, 1))  # adapt this if using `channels_first` image data format
 x_test = np.reshape(x_test, (len(x_test), pixels_amount, pixels_amount, 1))  # adapt this if using `channels_first` image data format
 print(x_train.shape)
-print(x_test.shape) """
+print(x_test.shape) 
 
 #if noisy images is wanted 
 """ 
@@ -106,35 +118,42 @@ x_test_noisy = np.clip(x_test_noisy, 0.0, 1.0) """
 
 #encoder model, to encode input into latent variable
 """ Convolution layers """
-conv_1 = Conv2D(16, (3, 3), activation='relu', padding='same')
+conv_1 = Conv2D(16, (3, 3), padding='same')
 pooling_1 = MaxPooling2D((2, 2), padding='same')
-conv_2 = Conv2D(8, (3, 3), activation='relu', padding='same')
+conv_2 = Conv2D(8, (3, 3), padding='same')
 pooling_2 = MaxPooling2D((2, 2), padding='same')
-conv_3 = Conv2D(8, (3, 3), activation='relu', padding='same')
+conv_3 = Conv2D(8, (3, 3), padding='same')
 pooling_3 = MaxPooling2D((2, 2), padding='same')
+lrelu = LeakyReLU()
 
 """ Flat layers """
-flat_hidden_1 =Dense(hidden_1_size, activation='relu')
-flat_hidden_2 = Dense(hidden_2_size, activation='relu')
+flat_hidden_1 =Dense(hidden_1_size)
+flat_hidden_2 = Dense(hidden_2_size)
 flat_2 = Dense(z_layer_size, activation='linear')
 flat_3 = Dense(z_layer_size, activation='linear')
 dropout_layer_encoder = Dropout(dropout_amount, input_shape=(hidden_1_size,)) #can be placed between the flat dense layers if needed
 """ Structure """
-inputs = Input(shape=(pixels_amount, pixels_amount, 1))
-x = conv_1(inputs)
+inputs_encoder = Input(shape=(pixels_amount, pixels_amount, 1))
+x = conv_1(inputs_encoder)
+x = LeakyReLU()(x)
 x = pooling_1(x)
 x = conv_2(x)
+x = LeakyReLU()(x)
 x = pooling_2(x)
 x = conv_3(x)
+x = LeakyReLU()(x)
 convoluted = pooling_3(x)
 
 convoluted_flat = Flatten()(convoluted)
 drop_encoder = dropout_layer_encoder(convoluted_flat)
 h_q =flat_hidden_1(drop_encoder)
-h_q_2 = flat_hidden_2(h_q)
-mu = flat_2(h_q_2)
-log_sigma = flat_3(h_q_2)
-encoder = Model(inputs, mu)
+lr1 = LeakyReLU()(h_q)
+h_q_2 = flat_hidden_2(lr1)
+#lr2 = LeakyReLU()(h_q_2)
+z = flat_2(h_q_2)
+#mu = flat_2(h_q_2)
+#log_sigma = flat_3(h_q_2)
+encoder = Model(inputs_encoder, z)
 
 print('encoder summary')
 encoder.summary()
@@ -148,10 +167,10 @@ _z = int(convShape[3])
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 #reparametrization trick to sample z 
-def sample_z(args):
+''' def sample_z(args):
     mu, log_sigma = args
     eps = K.random_normal(shape=(K.shape(mu)[0], z_layer_size), mean=0., stddev=1.)
-    return mu + K.exp(log_sigma / 2) * eps
+    return mu + K.exp(log_sigma / 2) * eps '''
 
 """ def sample_z(mu, log_sigma):
     # we sample from the standard normal a matrix of batch_size * latent_size (taking into account minibatches)
@@ -160,23 +179,23 @@ def sample_z(args):
     return mu + K.exp(log_sigma) * eps """
 
 #sample z Q(z|X)
-z = Lambda(sample_z)([mu, log_sigma])
+#z = Lambda(sample_z)([mu, log_sigma])
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 
 
 #decoder -- P(X|z) (generator) model, to generate new data given variable z
 """ Layers """
-decoder_hidden_2 = Dense(hidden_2_size, activation='relu')
+decoder_hidden_2 = Dense(hidden_2_size)
 dropout_layer_decoder = Dropout(dropout_amount, input_shape=(hidden_2_size,)) #can be placed between the flat dense layers if needed
-decoder_hidden = Dense(hidden_1_size, activation='relu')
-d_h = Dense(_x*_x*_z, activation='relu')
+decoder_hidden = Dense(hidden_1_size)
+d_h = Dense(_x*_x*_z)
 d_h_reshaped = Reshape((_x,_x,_z))
-decon_1 = Conv2D(8, (3, 3), activation='relu', padding='same')
+decon_1 = Conv2D(8, (3, 3), padding='same')
 upsamp_1 = UpSampling2D((2, 2))
-decon_2 = Conv2D(8, (3, 3), activation='relu', padding='same')
+decon_2 = Conv2D(8, (3, 3), padding='same')
 upsamp_2 = UpSampling2D((2, 2))
-decon_3 = Conv2D(16, (3, 3), activation='relu')#, padding='same'
+decon_3 = Conv2D(16, (3, 3), padding='same')#, padding='same'
 upsamp_3 = UpSampling2D((2, 2))
 decon_4 = Conv2D(1, (3, 3), activation='sigmoid', padding='same')
 
@@ -184,28 +203,38 @@ decon_4 = Conv2D(1, (3, 3), activation='sigmoid', padding='same')
 decon_5 = Conv2D(1, (3, 3), activation='sigmoid', padding='same') """
 
 """ Structure """
-d_in = Input(shape=(z_layer_size,))
-x = decoder_hidden_2(d_in)
+inputs_decoder = Input(shape=(z_layer_size,))
+x = decoder_hidden_2(inputs_decoder)
+x = LeakyReLU()(x)
 x = dropout_layer_decoder(x)
 x = decoder_hidden(x)
+x = LeakyReLU()(x)
 x = d_h(x)
+x = LeakyReLU()(x)
 x = d_h_reshaped(x)
 x = decon_1(x)
+x = LeakyReLU()(x)
 x = upsamp_1(x)
 x = decon_2(x)
+x = LeakyReLU()(x)
 x = upsamp_2(x)
 x = decon_3(x)
+x = LeakyReLU()(x)
 x = upsamp_3(x)
 """ x = decon_4(x)
 x =upsamp_4(x) """
-decode_output = decon_4(x)
-decoder = Model(d_in, decode_output)
+outputs_decoder = decon_4(x)
+decoder = Model(inputs_decoder, outputs_decoder)
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 
 #VAE model, for reconstruction and training
 """ Structure """
-x = decoder_hidden_2(z)
+inputs_ae = Input(shape=(pixels_amount, pixels_amount, 1))
+x = encoder(inputs_ae)
+outputs_ae = decoder(x)
+vae = Model(inputs_ae, outputs_ae)
+''' x = decoder_hidden_2(z)
 x = decoder_hidden(x)
 x = dropout_layer_decoder(x)
 x = d_h(x)
@@ -219,27 +248,15 @@ x = upsamp_3(x)
 """ x = decon_4(x)
 x = upsamp_4(x) """
 outputs = decon_4(x)
-vae = Model(inputs, outputs)
+vae = Model(inputs, outputs) '''
 
 
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
-#to load weights
-
-""" before_weight_load = vae.get_weights()
-vae.load_weights('./VariationalAE/saved_weights/weight.hdf5', by_name=False)
-after_weight_load = vae.get_weights()
-print('before_weight_load')
-print(before_weight_load)
-print('after_weight_load')
-print(after_weight_load) """
-#---------------------------------------------------------------------------------------------------------------------------------
-#----------------------------------------------
-
 #loss definition
 """ Flatten input and output layer to use in loss function """
-inputs_flat = Flatten()(inputs)
-outputs_flat = Flatten()(outputs)
+inputs_flat = Flatten()(inputs_encoder)
+outputs_flat = Flatten()(outputs_decoder)
 
 """ VAE loss function """
 vae_loss = reconstruction_loss = K.sum(K.square(outputs_flat-inputs_flat))
@@ -251,31 +268,39 @@ print(vae_loss)
 #----------------------------------------------
 #discriminator and discriminator encoder
 #layers
-disc_1 = Dense(disc_1_size, activation='relu')
-disc_2 = Dense(disc_2_size, activation='relu')
-disc_3 = Dense(disc_3_size, activation='relu')
-disc_4 = Dense(disc_4_size, activation='relu')
-disc_5 = Dense(disc_5_size, activation='relu')
+disc_1 = Dense(disc_1_size)
+disc_2 = Dense(disc_2_size)
+disc_3 = Dense(disc_3_size)
+disc_4 = Dense(disc_4_size)
+disc_5 = Dense(disc_5_size)
 disc_out = Dense(1, activation='sigmoid')
 
 #structure discriminator
 discriminator_input = Input(shape=(z_layer_size,))
 x = disc_1(discriminator_input)
+x = LeakyReLU()(x)
 x = disc_2(x)
+x = LeakyReLU()(x)
 x = disc_3(x)
+x = LeakyReLU()(x)
 x = disc_4(x)
+x = LeakyReLU()(x)
 #x = disc_5(x)
 discriminator_output = disc_out(x)
 discriminator = Model(discriminator_input, discriminator_output)
 
 #structure discriminatorencoder
-x = disc_1(z)
+inputs_disc_enc = Input(shape=(pixels_amount, pixels_amount, 1))
+x = encoder(inputs_disc_enc)
+output_disc_enc = discriminator(x)
+disc_enc = Model(inputs_disc_enc, output_disc_enc)
+''' x = disc_1(z)
 x = disc_2(x)
 x = disc_3(x)
 x = disc_4(x)
 #x = disc_5(x)
 discriminator_e_output = disc_out(x)
-discriminator_encoder = Model(inputs, discriminator_e_output)
+disc_enc = Model(inputs, discriminator_e_output) '''
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 
@@ -289,16 +314,37 @@ decoder.summary()
 print('discriminator')
 discriminator.summary()
 print('discriminator encoder')
-discriminator_encoder.summary()
+disc_enc.summary()
+#---------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------
+#to load weights
+def load_pretrained_weights():
+    vae.load_weights('./VariationalAE/saved_weights/vae_weights_epoch_{}.h5'.format(PREVIOUSLY_RUN_EPOCH), by_name=False)
+    disc_enc.load_weights('./VariationalAE/saved_weights/disc_enc_weights_epoch_{}.h5'.format(PREVIOUSLY_RUN_EPOCH), by_name=False)
+    encoder.load_weights('./VariationalAE/saved_weights/encoder_weights_epoch_{}.h5'.format(PREVIOUSLY_RUN_EPOCH), by_name=False)
+    decoder.load_weights('./VariationalAE/saved_weights/decoder_weights_epoch_{}.h5'.format(PREVIOUSLY_RUN_EPOCH), by_name=False)
+    discriminator.load_weights('./VariationalAE/saved_weights/discriminator_weights_epoch_{}.h5'.format(PREVIOUSLY_RUN_EPOCH), by_name=False)
+
+if LOAD_WEIGHTS_TO_MODEL==True:
+    load_pretrained_weights()
+
+
+""" before_weight_load = vae.get_weights()
+vae.load_weights('./VariationalAE/saved_weights/weight.hdf5', by_name=False)
+after_weight_load = vae.get_weights()
+print('before_weight_load')
+print(before_weight_load)
+print('after_weight_load')
+print(after_weight_load) """
 
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 #training
 discriminator.compile(optimizer='adam', loss='binary_crossentropy')
-discriminator_encoder.compile(optimizer='adam', loss='binary_crossentropy')
-vae.compile(optimizer='adam', loss='binary_crossentropy')
-encoder.compile(optimizer='adam', loss='binary_crossentropy')
-decoder.compile(optimizer='adam', loss='binary_crossentropy')
+disc_enc.compile(optimizer='adam', loss='binary_crossentropy')
+vae.compile(optimizer='adam', loss='mean_squared_error')
+''' encoder.compile(optimizer='adam', loss='binary_crossentropy')
+decoder.compile(optimizer='adam', loss='binary_crossentropy') '''
 
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
@@ -358,6 +404,15 @@ def imagegrid(dec, epochnumber):
         """ plt.show()
         plt.close(fig) """
 
+def saving_weights(epoch):
+    vae.save_weights('./VariationalAE/saved_weights/vae_weights_epoch_{}.h5'.format(epoch + PREVIOUSLY_RUN_EPOCH))
+    discriminator.save_weights('./VariationalAE/saved_weights/discriminator_weights_epoch_{}.h5'.format(epoch + PREVIOUSLY_RUN_EPOCH))
+    decoder.save_weights('./VariationalAE/saved_weights/decoder_weights_epoch_{}.h5'.format(epoch + PREVIOUSLY_RUN_EPOCH))
+    encoder.save_weights('./VariationalAE/saved_weights/encoder_weights_epoch_{}.h5'.format(epoch + PREVIOUSLY_RUN_EPOCH))
+    disc_enc.save_weights('./VariationalAE/saved_weights/disc_enc_weights_epoch_{}.h5'.format(epoch + PREVIOUSLY_RUN_EPOCH))
+
+
+
 def epoch_vis(self, epoch):
     n = 20  # figure with nxn images
     visualisation_size = pixels_amount
@@ -378,7 +433,7 @@ def epoch_vis(self, epoch):
     c.waitKey(1) """
     figure_to_file = figure*255 #reshape to get the right range when saving image to file
     figure_to_file = figure_to_file.astype('uint8')
-    name_of_image = './VariationalAE/epoch_plots/{}.jpg'.format(epoch)
+    name_of_image = './VariationalAE/epoch_plots/{}.jpg'.format(epoch + PREVIOUSLY_RUN_EPOCH)
     c.imwrite(name_of_image, figure_to_file)
 
 def settrainable(model, toset):
@@ -409,18 +464,20 @@ for epochnumber in range(n_epoch):
         discriminator.train_on_batch(discbatch_x, discbatch_y) #trains the discriminator to differ between wanted and unwanted (from encoder z) distribution
         
         """ training stage 3 -- encode z-values to fit in the wanted distribution """
-        settrainable(discriminator_encoder, True)
+        settrainable(disc_enc, True)
         settrainable(encoder, True)
         settrainable(discriminator, False) #using the weights from previous training stage (we just want to update the encoders weights)
-        discriminator_encoder.train_on_batch(batch, np.ones(batchsize)) #trains the encoder to code z-values to fit in the wanted distribution
+        disc_enc.train_on_batch(batch, np.ones(batchsize)) #trains the encoder to code z-values to fit in the wanted distribution
     
-    print(epochnumber, 'of' '{}'.format(n_epoch))
+    print('---------------------------------------------------------------- epoch',epochnumber + PREVIOUSLY_RUN_EPOCH, 'of ' '{}'.format(n_epoch + PREVIOUSLY_RUN_EPOCH))
     print("Reconstruction Loss:", vae.evaluate(x_train, x_train, verbose=1))
-    print("Adverserial Loss:", discriminator_encoder.evaluate(x_train, np.ones(len(x_train)), verbose=1))
+    print("Adverserial Loss:", disc_enc.evaluate(x_train, np.ones(len(x_train)), verbose=1))
 
-    if epochnumber % 10 == 0:
+    if epochnumber % SAVE_IMAGE_ON_EPOCH == 0:
         #imagegrid(decoder, epochnumber)
         epoch_vis(decoder, epochnumber)
+    if epochnumber % SAVE_WEIGHTS_ON_EPOCH == 0:
+        saving_weights(epochnumber)
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 #saving model and weights
@@ -429,10 +486,10 @@ for epochnumber in range(n_epoch):
 decoder.save('decoder_model.h5') """
 
 vae.save_weights('vae_weights.h5')
-discriminator.save_weights('discriminator_weight.h5')
+discriminator.save_weights('discriminator_weights.h5')
 decoder.save_weights('decoder_weights.h5')
 encoder.save_weights('encoder_weights.h5')
-discriminator_encoder.save_weights('discriminator_encoder_weight.h5')
+disc_enc.save_weights('disc_enc_weights.h5')
 #---------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------
 # visualisation
