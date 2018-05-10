c16738398ebcb741bf5a47ae955d7c1f799d863c
Alexander Nilsson <alexandernilsson12@gmail.com>
2018-05-10 02:34:43 +0200

    latent space interpolation

--- VAE/latent_space_exploration.py
+++ VAE/latent_space_exploration.py
@@ -1,12 +1,23 @@
 import os
 import cv2 as cv
 import numpy as np
+from datetime import datetime
 
 # Project Imports
+import model as M
 from model import config as C
 from model import architecture
+from visualization import saveImg, getLatentSpaceGrid
 
 PATH_TO_THIS_DIR = os.path.dirname(__file__)
+
+t = datetime.now()
+
+""" OUTPUT DIRECTORIES """
+# Create a new output directory for this training session with a unique name
+THIS_ID = "lsexp_{}--{}-{}-{}--{}-{}-{}".format(M.NAME, t.year, t.month, t.day, t.hour, t.minute, t.second)
+PATH_TO_OUT_DIRECTORY = os.path.join(PATH_TO_THIS_DIR, 'out', THIS_ID)
+os.makedirs(PATH_TO_OUT_DIRECTORY, exist_ok = True)
 
 PATH_TO_LOAD_WEIGHTS = os.path.join(PATH_TO_THIS_DIR, C.LOAD_FROM_DIRECTORY, 'saved_weights')
 
@@ -34,32 +45,68 @@ if C.PRINT_MODEL_SUMMARY:
 
 
 """ LOAD SAVED WEIGHTS """
-try:
-  before_weight_load = vae.get_weights()
-  vae.load_weights(os.path.join(PATH_TO_LOAD_WEIGHTS, 'weight.hdf5'), by_name=False)
-  after_weight_load = vae.get_weights()
+if True:
+  try:
+    before_weight_load = vae.get_weights()
+    vae.load_weights(os.path.join(PATH_TO_LOAD_WEIGHTS, 'weight.hdf5'), by_name=False)
+    after_weight_load = vae.get_weights()
 
-except Exception as e:
-  print('Failed to load stored weights..')
-  print(str(e))
+  except Exception as e:
+    print('Failed to load stored weights..')
+    print(str(e))
 
-else:
-  if np.array_equal(before_weight_load, after_weight_load):
-    print('Loaded weights are identical?')
   else:
-    print('Weights loaded successfully!')
-
+    if np.array_equal(before_weight_load, after_weight_load):
+      print('Loaded weights are identical?')
+    else:
+      print('Weights loaded successfully!')
 
 """ SAMPLE POINTS IN LATENT SPACE """
-N_SAMPLES = 1
+N_SAMPLES_PER_IMG = 36
+N_IMAGES = 100
 
-z_samples = np.random.normal(0, 1, (N_SAMPLES, C.Z_LAYER_SIZE))
+# Print Grids of randomly sampled points
+img_size = int(np.ceil(np.sqrt(N_SAMPLES_PER_IMG)) * C.IMG_SIZE)
 
-n_channels = 3 if C.COLOR_MODE == 'rgb' else 1
+""" PLOT RANDOM LATENT SPACE POINTS"""
+if False:
+  for i in range(N_IMAGES):
+    z_samples = np.random.normal(0, i+1, (N_SAMPLES_PER_IMG, C.Z_LAYER_SIZE))
 
-for z in z_samples:
-  # decode each z sample using the trained decoder
-  x = decoder.predict(z)
-  image = x[0].reshape(C.IMG_SIZE, C.IMG_SIZE, n_channels)
-  cv.imshow('Decoded Image', image)
-  cv.waitKey(0)
+    # Decode vectors and get image
+    img = getLatentSpaceGrid(decoder, z_samples, img_size = img_size)
+
+    # Save img
+    saveImg(img, PATH_TO_OUT_DIRECTORY, 'latent_space_exploration-{}.jpg'.format(i))
+
+
+""" PLOT 1 HOT LATENT VECTORS """
+if False:
+  # Generate 1-hot vectors
+  z_samples = np.eye(C.Z_LAYER_SIZE)
+
+  # Decode vectors and get image
+  img = getLatentSpaceGrid(decoder, z_samples, img_size = img_size)
+
+  # Save img
+  saveImg(img, PATH_TO_OUT_DIRECTORY, 'latent_space_exploration_1hot_vectors-{}.jpg'.format(1))
+
+
+""" PLOT RANDOM LATENT SPACE INTERPOLATION"""
+if True:
+  for i in range(N_IMAGES):
+    z_samples = np.random.normal(0, 2, (2, C.Z_LAYER_SIZE))
+    z_1, z_2 = z_samples[0], z_samples[1]
+
+    x = z_2 - z_1
+
+    z_samples = np.zeros((N_SAMPLES_PER_IMG, C.Z_LAYER_SIZE))
+
+    for j in range(N_SAMPLES_PER_IMG):
+      z_samples[j] = z_1 + x * (j+1)/N_SAMPLES_PER_IMG
+
+      # Decode vectors and get image
+      img = getLatentSpaceGrid(decoder, z_samples, img_size = img_size)
+
+      # Save img
+      saveImg(img, PATH_TO_OUT_DIRECTORY, 'latent_space_exploration-{}.jpg'.format(i))
